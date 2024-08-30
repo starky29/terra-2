267b93556f9ad81a4ea893ce63170cbc20970ae3
starky29 <andruwkakz@gmail.com>
2024-08-30 14:28:47 +0300

    Create vms_platform.tf

---
+++ vms_platform.tf
@@ -0,0 +1,53 @@
+variable "vm_web_vpc_name" {
+  type        = string
+  default     = "develop_web"
+  description = "VPC network & subnet name"
+}
+
+variable "vm_web_family" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "vm_web"
+}
+
+variable "vm_web_name" {
+  type        = string
+  default     = "netology-develop-platform-web"
+  description = "vm_web"
+}
+
+variable "vm_web_platform" {
+  type        = tuple([ string, number, number, number, bool, bool, bool ])
+  default     = ["standard-v3", 2, 1, 20, true, true, true]
+  description = "vm_web"
+}
+
+variable "vm_db_vpc_name" {
+  type        = string
+  default     = "develop_db"
+  description = "VPC network & subnet name"
+}
+
+variable "zone_b" {
+  type        = string
+  default     = "ru-central1-b"
+  description = "zone_b"
+}
+
+variable "vm_db_family" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "vm_db"
+}
+
+variable "vm_db_name" {
+  type        = string
+  default     = "netology-develop-platform-db"
+  description = "vm_db"
+}
+
+variable "vm_db_platform" {
+  type        = tuple([ string, number, number, number, bool, bool, bool ])
+  default     = ["standard-v3", 2, 2, 20, true, true, true]
+  description = "vm_db"
+}
